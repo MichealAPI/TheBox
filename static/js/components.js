0f65eef5f94f84daa29de8505281ef254ade57d7
Michele <michele.cippone01@gmail.com>
2024-12-23 23:38:39 +0100

    Extracted components for enhanced code reusability. Further improvements with the main stream

---
+++ static/js/components.js
@@ -0,0 +1,17 @@
+
+const componentsDir = '/static/components';
+
+function includeHTML(component, elementId) {
+    fetch(`${componentsDir}/${component}.html`)
+        .then(response => {
+            if (!response.ok) throw new Error(`Failed to load ${file}`);
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById(elementId).innerHTML = html;
+        })
+        .catch(err => console.error(err));
+}
+
+includeHTML('navbar', 'navbar');
+includeHTML('sidebar', 'sidebar');
